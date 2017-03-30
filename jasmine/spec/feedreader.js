5d790f40b185162d84b6c0dfaaf2e60b9b277154
Brian <justmats@gmail.com>
2017-03-29 21:38:05 -0400

    Write test that ensures loadFeed is called and completed and at least 1 entry shown

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -85,9 +85,17 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-    });
-    /* TODO: Write a new test suite named "New Feed Selection"
+         beforeEach(function (done) {
+            loadFeed(0,done);
+         });
 
+         it('one entry in feed container', function(done) {
+            expect($('.feed').children().length).not.toBe(0);
+            done();
+         });
+    });
+    /* TODO: Write a new test suite named "New Feed Selection"*/
+    
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
